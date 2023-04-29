a153286236fb850396b9b65cbfa26760f949e0dd
tazeen <tazeen_86@hotmail.com>
2023-04-29 14:14:10 +1000

    Chatbot trained on netflix data  commit

---
+++ cb2.py
@@ -0,0 +1,42 @@
+from gpt_index import ServiceContext,SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from langchain.chat_models import ChatOpenAI
+import gradio as gr
+import sys
+import os 
+
+from dotenv import load_dotenv
+
+load_dotenv()
+os.environ["OPENAI_API_KEY"] = os.environ.get('OPENAI_API_KEY')
+
+def construct_index(directory_path):
+    max_input_size = 4096
+    num_outputs = 512
+    max_chunk_overlap = 20
+    chunk_size_limit = 600
+
+    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+
+    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.1, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
+
+    documents = SimpleDirectoryReader(directory_path).load_data()
+
+      
+    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor,prompt_helper=prompt_helper)
+    index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
+    index.save_to_disk('index.json')
+
+    return index
+
+def chatbot(input_text):
+    index = GPTSimpleVectorIndex.load_from_disk('index.json')
+    response = index.query(input_text, response_mode="compact")
+    return response.response
+
+iface = gr.Interface(fn=chatbot,
+                     inputs=gr.components.Textbox(lines=7, label="Enter your text"),
+                     outputs="text",
+                     title="Custom-trained AI Chatbot")
+
+index = construct_index("docs")
+iface.launch(share=True)
